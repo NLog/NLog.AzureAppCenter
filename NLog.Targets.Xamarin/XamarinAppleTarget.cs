2a02d7efada2ec3034d7c1cb3a38247093a848b9
Rolf Kristensen <sweaty1@hotmail.com>
2022-08-27 14:35:13 +0200

    NLog.Targets.Xamarin

---
+++ NLog.Targets.Xamarin/XamarinAppleTarget.cs
@@ -0,0 +1,78 @@
+﻿#if __APPLE__
+
+using System;
+using NLog.Layouts;
+
+namespace NLog.Targets.Xamarin
+{
+    /// <summary>
+    /// Output target for Apple Unified Logging
+    /// </summary>
+    [Target("DebugXamarin")]
+    internal class XamarinAppleTarget : TargetWithLayoutHeaderAndFooter
+    {
+        /// <summary>
+        /// Not used at the moment
+        /// </summary>
+        public Layout Category { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="XamarinAppleTarget"/> class.
+        /// </summary>
+        public XamarinAppleTarget()
+        {
+            Layout = "${logger}|${message:withException=true:exceptionSeparator=|}";
+        }
+
+        /// <inheritdoc/>
+        protected override void InitializeTarget()
+        {
+            base.InitializeTarget();
+
+            if (Header != null)
+            {
+                var logEvent = LogEventInfo.CreateNullEvent();
+                logEvent.Level = LogLevel.Info;
+                logEvent.LoggerName = "Starting";
+                DebugWriteLine(Header, logEvent);
+            }
+        }
+
+        /// <inheritdoc/>
+        protected override void CloseTarget()
+        {
+            if (Footer != null)
+            {
+                var logEvent = LogEventInfo.CreateNullEvent();
+                logEvent.Level = LogLevel.Info;
+                logEvent.LoggerName = "Closing";
+                DebugWriteLine(Footer, logEvent);
+            }
+
+            base.CloseTarget();
+        }
+
+        private void DebugWriteLine(Layout layout, LogEventInfo logEvent)
+        {
+            var logMessage = RenderLogEvent(layout, logEvent) ?? string.Empty;
+            if (logEvent.Level == LogLevel.Trace || logEvent.Level == LogLevel.Debug)
+            {
+                CoreFoundation.OSLog.Default.Log(CoreFoundation.OSLogLevel.Debug, logMessage);
+            }
+            else if (logEvent.Level == LogLevel.Info)
+            {
+                CoreFoundation.OSLog.Default.Log(CoreFoundation.OSLogLevel.Info, logMessage);
+            }
+            else if (logEvent.Level == LogLevel.Warn || logEvent.Level == LogLevel.Error)
+            {
+                CoreFoundation.OSLog.Default.Log(CoreFoundation.OSLogLevel.Error, logMessage);
+            }
+            else
+            {
+                CoreFoundation.OSLog.Default.Log(CoreFoundation.OSLogLevel.Fault, logMessage);
+            }
+        }
+    }
+}
+
+#endif
---
+++ NLog.Targets.Xamarin/XamarinDebuggerTarget.cs
@@ -0,0 +1,77 @@
+﻿#if !__APPLE__ && !__ANDROID__
+
+using System.Diagnostics;
+using NLog.Layouts;
+
+namespace NLog.Targets.Xamarin
+{
+    /// <summary>
+    /// Output target for Android.Util.Log
+    /// </summary>
+    [Target("DebugXamarin")]
+    public class XamarinDebuggerTarget : TargetWithLayoutHeaderAndFooter
+    {
+        /// <summary>
+        /// The category of the message
+        /// </summary>
+        public Layout Category { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="XamarinDebuggerTarget"/> class.
+        /// </summary>
+        public XamarinDebuggerTarget()
+        {
+            Layout = "${level}|${message:withException=true:exceptionSeparator=|}";
+            Category = "${logger}";
+        }
+
+        /// <inheritdoc/>
+        protected override void InitializeTarget()
+        {
+            base.InitializeTarget();
+
+            if (Header != null && Debugger.IsLogging())
+            {
+                var logEvent = LogEventInfo.CreateNullEvent();
+                logEvent.Level = LogLevel.Info;
+                logEvent.LoggerName = "Starting";
+                DebugWriteLine(Header, logEvent);
+            }
+        }
+
+        /// <inheritdoc/>
+        protected override void CloseTarget()
+        {
+            if (Footer != null && Debugger.IsLogging())
+            {
+                var logEvent = LogEventInfo.CreateNullEvent();
+                logEvent.Level = LogLevel.Info;
+                logEvent.LoggerName = "Closing";
+                DebugWriteLine(Footer, logEvent);
+            }
+
+            base.CloseTarget();
+        }
+
+        /// <inheritdoc/>
+        protected override void Write(LogEventInfo logEvent)
+        {
+            if (Debugger.IsLogging())
+            {
+                DebugWriteLine(Layout, logEvent);
+            }
+        }
+
+        private void DebugWriteLine(Layout layout, LogEventInfo logEvent)
+        {
+            var logMessage = RenderLogEvent(layout, logEvent) ?? string.Empty;
+            var logCategory = RenderLogEvent(Category, logEvent);
+            if (string.IsNullOrEmpty(logCategory))
+                logCategory = null;
+
+            Debugger.Log(logEvent.Level.Ordinal, logCategory, logMessage + System.Environment.NewLine);
+        }
+    }
+}
+
+#endif
