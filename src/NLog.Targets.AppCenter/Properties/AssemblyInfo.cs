04b0b6660cdb3ada197981629f33a29ab5f793d9
Rolf Kristensen <sweaty1@hotmail.com>
2022-11-26 13:41:29 +0100

    Version 5.0

--- src/NLog.Targets.AppCenter/Properties/AssemblyInfo.cs
+++ src/NLog.Targets.AppCenter/Properties/AssemblyInfo.cs
@@ -1,3 +1,8 @@
 using System.Runtime.CompilerServices;
+using System.Security;
+using NLog.Targets.AppCenter;
 
+[assembly: Preserve]    // Automatic --linkskip=NLog.Targets.AppCenter
+[assembly: AllowPartiallyTrustedCallers]
+[assembly: SecurityRules(SecurityRuleSet.Level1)]
 [assembly: InternalsVisibleTo("NLog.Targets.AppCenter.Tests")]
